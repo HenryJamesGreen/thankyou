345dc503a8f9fa2f0cfbc4106147e6c1c21300ec
jh871 <jennyh871@gmail.com>
2023-03-30 18:56:17 +0100

    added home page with fun styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-import React from "react";
+import './Home.css'
 
-function Home() {
-  return (
-    <div className="home-container">
-      <h1>Welcome to My React App</h1>
-      <p>This is the home page.</p>
-    </div>
-  );
+
+export default function Home() {
+    return(
+        <>
+        <div className="jumbo-div">
+            <h2 className='intro'> Your favourite bootcamp class wants to say </h2>
+            <h3 className="thanks-type">thank you!</h3>
+        </div>
+        <div className='wrapper'>
+        <div className='body'>
+            <h2 className='header'> To Nirmeet, Chelsea, Chadrick, John, Austin, Other Austin, Lee, Matthew, Paul and Laura</h2>
+                <p className='message'>we've knocked up a quick something to let you all know how grateful we are for all of your help on this journey.
+        <br /> <br />
+                We couldn't have done it without you. 
+                <br /> <br />
+                We owe you a million thank yous. But that would take too long so {/*put a link here to thanks page*/}here's just a few.
+                </p>
+        </div>
+        </div>
+    </> )
 }
-
-export default Home;
